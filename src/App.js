3b27d0df1f0fd91cb2d7eeb2a1bd60850fd20631
ashmeetsmonga <ashmeetsmonga@gmail.com>
2022-05-19 19:10:17 +0530

    added words list and solution state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+import getRandomWord from "./wordList";
+
 function App() {
+	const [solution, setSolution] = useState(null);
+
+	useEffect(() => {
+		const word = getRandomWord();
+		console.log(word);
+		setSolution(word);
+	}, [setSolution]);
+
 	return (
-		<div className='App'>
+		<div className='flex flex-col items-center w-screen'>
 			<h1 className='text-xl'>Wordle Clone</h1>
+			{solution && <p>The solution is : {solution}</p>}
 		</div>
 	);
 }
